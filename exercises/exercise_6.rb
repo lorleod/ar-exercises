1d0017eef0b6224725b076ae9ede188c8d70c2a0
lorleod <taylor.mcleod@gmail.com>
2022-06-28 04:43:44 +0000

    completed up to 5

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -23,15 +23,3 @@ puts "----------"
 # Add the following code directly inside the Employee model (class): belongs_to :store
 # Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 # Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
-# Exercise 7: Validations for both models
-# Add validations to two models to enforce the following business rules:
-# Employees must always have a first name present
-# Employees must always have a last name present
-# Employees have a hourly_rate that is a number (integer) between 40 and 200
-# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
-# Stores must always have a name that is a minimum of 3 characters
-# Stores have an annual_revenue that is a number (integer) that must be 0 or more
-# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
-# Ask the user for a store name (store it in a variable)
-# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
-# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
